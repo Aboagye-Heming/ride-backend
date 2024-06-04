2914063c13abf9e55ef30b4b3ef0d4eef5da8db6
Aboagye=Heming <hemingkwabena@gmail.com>
2024-06-04 20:14:04 +0000

    added a driver controller

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'submit']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::group(['middleware'=> 'auth:sanctum'], function() {
+    Route::get('/driver', [DriverController::class, "show"]);
+    Route::get('/driver', [DriverController::class, "update"]);
+    Route::post('/driver', []);
+
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+    
+});
